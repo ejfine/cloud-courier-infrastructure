e30aded3101c2b72c15d9e97f94b2b8f142bb20f
Eli Fine <ejfine@gmail.com>
2025-02-05 22:24:57 +0000

    pylint

--- src/cloud_courier_infrastructure/hybrid_activation.py
+++ src/cloud_courier_infrastructure/hybrid_activation.py
@@ -1,7 +1,5 @@
 from ephemeral_pulumi_deploy import append_resource_suffix
 from pulumi import ComponentResource
-from pulumi import ResourceOptions
-from pulumi_aws_native import s3
 
 
 class OnPremNode(ComponentResource):
@@ -13,11 +11,3 @@ class OnPremNode(ComponentResource):
             append_resource_suffix("raw-data-bucket"),
             None,
         )
-        bucket = s3.Bucket(
-            append_resource_suffix("raw-data-bucket"),
-            opts=ResourceOptions(parent=self),
-            versioning_configuration=s3.BucketVersioningConfigurationArgs(
-                status=s3.BucketVersioningConfigurationStatus.ENABLED
-            ),
-        )
-        self.bucket_name = bucket.id
