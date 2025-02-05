c17fcc053ed3a9d603890f9a1ab1f6fadc9c7a5e
Eli Fine <ejfine@gmail.com>
2025-02-05 21:36:35 +0000

    bucket

--- src/cloud_courier_infrastructure/bucket.py
+++ src/cloud_courier_infrastructure/bucket.py
@@ -1,5 +1,7 @@
 from ephemeral_pulumi_deploy import append_resource_suffix
 from pulumi import ComponentResource
+from pulumi import ResourceOptions
+from pulumi_aws_native import s3
 
 
 class RawDataBucket(ComponentResource):
@@ -11,3 +13,4 @@ class RawDataBucket(ComponentResource):
             append_resource_suffix("raw-data-bucket"),
             None,
         )
+        _ = s3.Bucket(append_resource_suffix("raw-data-bucket"), opts=ResourceOptions(parent=self))
