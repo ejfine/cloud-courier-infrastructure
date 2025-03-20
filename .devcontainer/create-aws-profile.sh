991584ab30262d06d1964bb68b8eb7b1cf47c488
Eli Fine <eli88fine@gmail.com>
2025-03-20 18:28:55 +0000

    copier update

--- .devcontainer/create-aws-profile.sh
+++ .devcontainer/create-aws-profile.sh
@@ -2,6 +2,13 @@
 set -ex
 
 mkdir -p ~/.aws
+
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  LOCALSTACK_ENDPOINT_URL="http://localhost:4566"
+else
+  LOCALSTACK_ENDPOINT_URL="http://localstack:4566"
+fi
+
 cat >> ~/.aws/config <<EOF
 [profile development]
 sso_session = org
@@ -17,7 +24,9 @@ sso_registration_scopes = sso:account:access
 [profile localstack]
 region=us-east-1
 output=json
-endpoint_url = http://localstack:4566
+endpoint_url = $LOCALSTACK_ENDPOINT_URL
+
+
 EOF
 cat >> ~/.aws/credentials <<EOF
 [localstack]
